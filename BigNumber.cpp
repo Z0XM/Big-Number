a1233c19080c6ff24c80997b17773060fe886013
Mukul <mukulz2020@gmail.com>
2021-06-27 11:59:28 +0530

    fixed bugs, revamped code

--- BigNumber.cpp
+++ BigNumber.cpp
@@ -7,50 +7,119 @@
 
 int toDigit(char c) { return c - 48; }
 char toChar(int c) { return c + 48; }
-template <typename T = int> std::string toString(const T& x) {
-	std::ostringstream out; out << x; return out.str(); 
+int pow10(int x) { int n = 1; for (int i = 0; i < x; i++)n *= 10; return n; }
+
+std::string trailZeroesFront(const std::string& num)
+{
+	bool neg = num[0] == '-';
+	int start = neg;
+	for (; num[start] == '0' && start < num.size(); start++);
+	if (start == num.size())return "0";
+	return num.substr(start);
 }
-template <typename begin_it, typename end_it>
+
+std::string trailZeroesBack(const std::string& num)
+{
+	int count;
+	for (count = 0; count < num.size() && num[num.size() - count - 1] == '0'; count++);
+	if (count == num.size())return "0";
+	return num.substr(0, num.size() - count);
+}
+
+void fillVec(std::vector<BigNumber::Int>& vec, int x)
+{
+	vec.clear();
+	while (x != 0) {
+		vec.push_back(floor(x % BigNumber::EXP_OF_10));
+		x /= BigNumber::EXP_OF_10;
+	}
+}
+
+template<typename begin_it, typename end_it>
 std::string toString(begin_it begin, end_it end, bool fractional = false) {
-	std::ostringstream out; 
+	std::ostringstream out;
 	for (auto i = begin; i != end; i++) {
-		if (i != begin || fractional)for (int ten = 1000; ten != 1 && *i < ten; ten /= 10)out << '0';
+		if (i != begin || fractional)
+			for (int ten = BigNumber::EXP_OF_10/10; ten != 1 && *i < ten; ten /= 10)
+				out << '0';
 		out << *i;
 	}
 	return out.str();
 }
 
-std::string& trailZeroes(std::string&);
-std::string& trailZeroesIntegral(std::string&);
-std::string& trailZeroesFractional(std::string&);
-
 BigNumber::BigNumber()
 {
-	integral.vec.push_back(0);
-	integral.str = "0";
-	fractional.vec.push_back(0);
-	fractional.str = "0";
-	negative = false;
 }
 
-BigNumber::BigNumber(int x)
+BigNumber::BigNumber(int x) 
 {
-	setNumber(x);
+	negative = x < 0;
+	x = abs(x);
+
+	integral.str = std::to_string(x);
+	fillVec(integral.vec, x);
 }
 
 BigNumber::BigNumber(double x)
 {
-	setNumber(x);
+	negative = x < 0;
+	x = abs(x);
+	int n = floor(x);
+
+	integral.str = std::to_string(n);
+	fillVec(integral.vec, n);
+
+	fractional.str = trailZeroesBack(std::to_string(x - n));
+	fractional.str.erase(0, 2);
+	fillVec(fractional.vec, (x - n) * pow10(fractional.str.size()));
+
 }
 
-BigNumber::BigNumber(std::string num)
+BigNumber::BigNumber(const std::string& num)
 {
-	setNumber(trailZeroes(num));
+	std::string str = num;
+	
+	if (str[0] == '-') {
+		str.erase(0, 1);
+		negative = true;
+	}
+
+	int k = str.find('.');
+	if (k != str.npos) {
+		str[k] = ' ';
+	}
+
+	std::stringstream ss(str);
+	ss >> integral.str >> fractional.str;
+
+	integral.str = trailZeroesFront(integral.str);
+
+	integral.vec.clear();
+	for (int i = 0; i < integral.str.size(); i++) {
+		if (i % POWER_OF_10 == 0)integral.vec.push_back(0);
+		integral.vec.back() += toDigit(integral.str[integral.str.size() - 1 - i]) * (int)pow(10, i % POWER_OF_10);
+	}
+
+	fractional.str = trailZeroesBack(fractional.str);
+
+	fractional.vec.clear();
+	for (int i = 0; i < fractional.str.size(); i++) {
+		if (i % POWER_OF_10 == 0)fractional.vec.push_back(0);
+		fractional.vec.back() += toDigit(fractional.str[i]) * (int)pow(10, (POWER_OF_10 - i % POWER_OF_10 - 1));
+	}
 }
 
 BigNumber::~BigNumber()
 {
-	
+}
+
+std::string BigNumber::asString() const
+{
+	std::string result;
+	if (negative) result += '-';
+	result += integral.str;
+	if (fractional.str != "0")result += '.' + fractional.str;
+	return result;
 }
 
 std::ostream& operator<<(std::ostream& out, const BigNumber& nBig)
@@ -67,108 +136,6 @@ std::ostream& operator<<(std::ostream& out, const BigNumber& nBig)
 	return out;
 }
 
-std::string BigNumber::asString() const
-{
-	std::string result;
-	if (negative) result += '-';
-	result += integral.str;
-	if(fractional.str != "0")result += '.' + fractional.str;
-	return result;
-}
-
-void BigNumber::setNumber(const int& x)
-{
-	integral.str = toString(abs(x));
-	integral.vec.clear();
-	integral.vec.push_back(abs(x) % POWER_OF_10);
-	if (abs(x) > MAX_NUM) {
-		integral.vec.push_back(floor(abs(x) / POWER_OF_10));
-	}
-	
-	fractional.vec.clear();
-	fractional.vec.push_back(0);
-	fractional.str = "0";
-
-	negative = x < 0;
-}
-
-void BigNumber::setNumber(const double& x)
-{
-	integral.str = toString(floor(abs(x)));
-	integral.vec.clear();
-	integral.vec.push_back((int)(abs(x)) % POWER_OF_10);
-	if (x > MAX_NUM) {
-		integral.vec.push_back(floor(abs(x) / POWER_OF_10));
-	}
-
-	fractional.str = toString(abs(x) - floor(abs(x)));
-	fractional.str.erase(0, 2);
-	fractional.vec.clear();
-	fractional.vec.push_back((abs(x) - floor(abs(x))) * (int)pow(10, fractional.str.size()));
-
-	negative = x < 0;
-}
-
-void BigNumber::setNumber(const std::string& num)
-{
-	negative = num[0] == '-';
-
-	bool isDecimal = num.find('.') != num.npos;
-	integral.str = isDecimal ? num.substr(negative, num.find('.') - negative) : num.substr(negative, num.size() - negative);
-
-	integral.vec.clear();
-	for (int i = 0; i < integral.str.size(); i++) {
-		if (i % 4 == 0)integral.vec.push_back(0);
-		integral.vec.back() += toDigit(integral.str[integral.str.size() - 1 - i]) * (int)pow(10, i % 4);
-	}
-	if (integral.vec.empty()) {
-		integral.vec.push_back(0);
-		integral.str = "0";
-	}
-
-	fractional.str = isDecimal ? num.substr(num.find('.') + 1, num.size() - negative) : "0";
-	
-	fractional.vec.clear();
-	for (int i = 0; i < fractional.str.size(); i++) {
-		if (i % 4 == 0)fractional.vec.push_back(0);
-		fractional.vec.back() += toDigit(fractional.str[i]) * (int)pow(10, (4 - i%4 - 1));
-	}
-	if (fractional.vec.empty()) {
-		fractional.vec.push_back(0);
-		fractional.str = "0";
-	}
-}
-
-std::string& trailZeroes(std::string& num)
-{
-	trailZeroesIntegral(num);
-
-	bool isDecimal = num.find('.') != num.npos;
-
-	if (!isDecimal)return num;
-
-	trailZeroesFractional(num);
-	
-	return num;
-}
-
-std::string& trailZeroesIntegral(std::string& num)
-{
-	bool neg = num[0] == '-';
-	int count = neg;
-	for (; num[count] == '0'; count++);
-	num.erase(neg, count - neg);
-	return num;
-}
-
-std::string& trailZeroesFractional(std::string& num)
-{
-	int index;
-	for (index = num.size() - 1; index >=0 && num[index] == '0'; index--);
-	num.erase(index + 1);
-	return num;
-}
-
 bool operator==(const BigNumber& nBig1, const BigNumber& nBig2)
 {
 	return nBig1.integral.str == nBig2.integral.str && nBig1.fractional.str == nBig2.fractional.str && nBig1.negative == nBig2.negative;
@@ -176,30 +143,31 @@ bool operator==(const BigNumber& nBig1, const BigNumber& nBig2)
 
 bool operator!=(const BigNumber& nBig1, const BigNumber& nBig2)
 {
-	return nBig1.integral.str != nBig2.integral.str && nBig1.fractional.str != nBig2.fractional.str && nBig1.negative != nBig2.negative;
+	return !(nBig1 == nBig2);
 }
+
 bool operator<(const BigNumber& nBig1, const BigNumber& nBig2)
 {
 	if (nBig1.negative > nBig2.negative)return true;
 	if (nBig1.negative < nBig2.negative)return false;
 
-	if (nBig1.integral.vec.size() < nBig2.integral.vec.size())return true;
-	if (nBig1.integral.vec.size() > nBig2.integral.vec.size())return false;
+	if (nBig1.integral.str.size() < nBig2.integral.str.size())return !nBig1.negative;
+	if (nBig1.integral.str.size() > nBig2.integral.str.size())return nBig1.negative;
 
 	for (int i = nBig1.integral.vec.size() - 1; i >= 0; i--)
 	{
-		if (nBig1.integral.vec[i] < nBig2.integral.vec[i])return true;
-		if (nBig1.integral.vec[i] > nBig2.integral.vec[i])return false;
+		if (nBig1.integral.vec[i] < nBig2.integral.vec[i])return !nBig1.negative;
+		if (nBig1.integral.vec[i] > nBig2.integral.vec[i])return nBig1.negative;
 	}
 
 	for (int i = 0; i < nBig1.fractional.vec.size(); i++)
 	{
-		if (i == nBig2.fractional.vec.size())return false;
-		if (nBig1.fractional.vec[i] < nBig2.fractional.vec[i])return true;
-		if (nBig1.fractional.vec[i] > nBig2.fractional.vec[i])return false;
+		if (i == nBig2.fractional.vec.size())return nBig1.negative;
+		if (nBig1.fractional.vec[i] < nBig2.fractional.vec[i])return !nBig1.negative;
+		if (nBig1.fractional.vec[i] > nBig2.fractional.vec[i])return nBig1.negative;
 	}
 
-	return false;
+	return nBig1.negative;
 }
 
 bool operator>(const BigNumber& nBig1, const BigNumber& nBig2)
@@ -214,7 +182,7 @@ bool operator<=(const BigNumber& nBig1, const BigNumber& nBig2)
 
 bool operator>=(const BigNumber& nBig1, const BigNumber& nBig2)
 {
-	return nBig2 < nBig1 || nBig1 == nBig2;
+	return nBig1 > nBig2 || nBig1 == nBig2;
 }
 
 BigNumber operator+(const BigNumber& nBig1, const BigNumber& nBig2)
@@ -236,12 +204,12 @@ BigNumber operator+(const BigNumber& nBig1, const BigNumber& nBig2)
 
 	nResult.fractional.vec.clear();
 	nResult.fractional.vec.reserve(std::max(nBig1.fractional.vec.size(), nBig2.fractional.vec.size()));
-	for (int i = std::max(nBig1.fractional.vec.size(), nBig2.fractional.vec.size()) - 1; i >= 0; i--) 
+	for (int i = std::max(nBig1.fractional.vec.size(), nBig2.fractional.vec.size()) - 1; i >= 0; i--)
 	{
 		int num[2] = { 0, 0 };
 		if (i < nBig1.fractional.vec.size())num[0] = nBig1.fractional.vec[i];
 		if (i < nBig2.fractional.vec.size())num[1] = nBig2.fractional.vec[i];
-		nResult.fractional.vec.insert(nResult.fractional.vec.begin(), (num[0] + num[1] + carry) % BigNumber::POWER_OF_10);
+		nResult.fractional.vec.insert(nResult.fractional.vec.begin(), (num[0] + num[1] + carry) % BigNumber::EXP_OF_10);
 		carry = (num[0] + num[1] + carry) > BigNumber::MAX_NUM;
 	}
 	if (nResult.fractional.vec.empty()) {
@@ -249,18 +217,17 @@ BigNumber operator+(const BigNumber& nBig1, const BigNumber& nBig2)
 		nResult.fractional.str = "0";
 	}
 	else {
-		nResult.fractional.str = toString(nResult.fractional.vec.cbegin(), nResult.fractional.vec.cend());
-		trailZeroesFractional(nResult.fractional.str);
+		nResult.fractional.str = toString(nResult.fractional.vec.cbegin(), nResult.fractional.vec.cend(), true);
+		nResult.fractional.str = trailZeroesBack(nResult.fractional.str);
 	}
 
 	nResult.integral.vec.clear();
-
 	for (int i = 0; i < nBig1.integral.vec.size() || i < nBig2.integral.vec.size(); i++)
 	{
 		int num[2] = { 0, 0 };
 		if (i < nBig1.integral.vec.size())num[0] = nBig1.integral.vec[i];
 		if (i < nBig2.integral.vec.size())num[1] = nBig2.integral.vec[i];
-		nResult.integral.vec.push_back((num[0] + num[1] + carry) % BigNumber::POWER_OF_10);
+		nResult.integral.vec.push_back((num[0] + num[1] + carry) % BigNumber::EXP_OF_10);
 		carry = (num[0] + num[1] + carry) > BigNumber::MAX_NUM;
 	}
 	if (carry)nResult.integral.vec.push_back(1);
@@ -301,6 +268,7 @@ BigNumber operator-(const BigNumber& nBig1, const BigNumber& nBig2)
 	bool carry = 0;
 
 	nResult.fractional.vec.clear();
+	
 
 	for (int i = nBig1.fractional.vec.size() - 1; i >= 0; i--)
 	{
@@ -308,9 +276,10 @@ BigNumber operator-(const BigNumber& nBig1, const BigNumber& nBig2)
 		if (i < nBig2.fractional.vec.size())num[1] = nBig2.fractional.vec[i];
 		if (num[0] < num[1]) {
 			carry = 1;
-			num[0] += BigNumber::POWER_OF_10;
+			num[0] += BigNumber::EXP_OF_10;
 		}
-		nResult.fractional.vec.insert(nResult.fractional.vec.begin(), (num[0] - num[1]) % BigNumber::POWER_OF_10);
+		else carry = 0;
+		nResult.fractional.vec.insert(nResult.fractional.vec.begin(), (num[0] - num[1]) % BigNumber::EXP_OF_10);
 	}
 	if (nResult.fractional.vec.empty()) {
 		nResult.fractional.vec.push_back(0);
@@ -318,21 +287,21 @@ BigNumber operator-(const BigNumber& nBig1, const BigNumber& nBig2)
 	}
 	else {
 		nResult.fractional.str = toString(nResult.fractional.vec.cbegin(), nResult.fractional.vec.cend(), true);
-		trailZeroesFractional(nResult.fractional.str);
+		nResult.fractional.str = trailZeroesBack(nResult.fractional.str);
 	}
 
 	nResult.integral.vec.clear();
 
 	for (int i = 0; i < nBig1.integral.vec.size(); i++)
 	{
-		int num[2] = { nBig1.integral.vec[i], 0 };
+		int num[2] = { nBig1.integral.vec[i] - carry, 0 };
 		if (i < nBig2.integral.vec.size())num[1] = nBig2.integral.vec[i];
 		if (num[0] < num[1]) {
 			carry = 1;
-			num[0] += BigNumber::POWER_OF_10;
+			num[0] += BigNumber::EXP_OF_10;
 		}
 		else carry = 0;
-		nResult.integral.vec.push_back((num[0] - num[1]) % BigNumber::POWER_OF_10);
+		nResult.integral.vec.push_back((num[0] - num[1]) % BigNumber::EXP_OF_10);
 	}
 	if (carry)nResult.integral.vec.push_back(1);
 	if (nResult.integral.vec.empty()) {
@@ -356,32 +325,32 @@ BigNumber operator*(const BigNumber& nBig1, const BigNumber& nBig2)
 
 		while (nResult.fractional.vec.size() != nBig1.fractional.vec.size() + nBig2.fractional.vec.size())nResult.fractional.vec.push_back(0);
 
-		for (int i = 0; i < nBig1.fractional.vec.size(); i++){
-			for (int j = 0; j < nBig2.fractional.vec.size(); j++){
+		for (int i = 0; i < nBig1.fractional.vec.size(); i++) {
+			for (int j = 0; j < nBig2.fractional.vec.size(); j++) {
 				int cell = i + j;
 
 				carry = nBig1.fractional.vec[i] * nBig2.fractional.vec[j];
-				while (carry % BigNumber::POWER_OF_10 == 0)carry /= BigNumber::POWER_OF_10;
+				while (carry % BigNumber::EXP_OF_10 == 0)carry /= BigNumber::EXP_OF_10;
 
-				if ((carry / BigNumber::POWER_OF_10) != 0)cell++;
+				if ((carry / BigNumber::EXP_OF_10) != 0)cell++;
 
 				do {
 					int sum = nResult.fractional.vec[cell] + carry;
-					nResult.fractional.vec[cell] = sum % BigNumber::POWER_OF_10;
-					carry = sum / BigNumber::POWER_OF_10;
+					nResult.fractional.vec[cell] = sum % BigNumber::EXP_OF_10;
+					carry = sum / BigNumber::EXP_OF_10;
 					cell--;
 				} while (carry && cell >= 0);
 			}
 		}
 		while (nResult.fractional.vec.back() == 0)nResult.fractional.vec.pop_back();
 		nResult.fractional.str = toString(nResult.fractional.vec.cbegin(), nResult.fractional.vec.cend(), true);
-		trailZeroesFractional(nResult.fractional.str);
+		nResult.fractional.str = trailZeroesBack(nResult.fractional.str);
 	}
-
+	
 	auto integralFractionalMultiply = [&nResult](const std::string& first, const std::string& second) {
 		std::string num = (BigNumber(first) * BigNumber(second)).asString();
-		while (second.size() > num.size())num += '0';
-		if (second.size() == num.size())num += "0.";
+		while (second.size() > num.size())num = "0" + num;
+		if (second.size() == num.size())num = "0." + num;
 		else num.insert(num.cend() - second.size(), '.');
 		nResult = nResult + BigNumber(num);
 	};
@@ -392,7 +361,7 @@ BigNumber operator*(const BigNumber& nBig1, const BigNumber& nBig2)
 	if (nBig1.integral.str != "0" && nBig2.integral.str != "0") {
 		if (nBig1.integral.str == "0" || nBig2.integral.str == "0")return nResult;
 
-		for (int i = 0; i < nBig1.integral.vec.size(); i++){
+		for (int i = 0; i < nBig1.integral.vec.size(); i++) {
 			for (int j = 0; j < nBig2.integral.vec.size(); j++) {
 				int cell = i + j;
 
@@ -402,8 +371,8 @@ BigNumber operator*(const BigNumber& nBig1, const BigNumber& nBig2)
 				do {
 					if (cell >= nResult.integral.vec.size())nResult.integral.vec.push_back(0);
 					int sum = nResult.integral.vec[cell] + carry;
-					nResult.integral.vec[cell] = sum % BigNumber::POWER_OF_10;
-					carry = sum / BigNumber::POWER_OF_10;
+					nResult.integral.vec[cell] = sum % BigNumber::EXP_OF_10;
+					carry = sum / BigNumber::EXP_OF_10;
 					cell++;
 				} while (carry);
 			}
@@ -412,9 +381,5 @@ BigNumber operator*(const BigNumber& nBig1, const BigNumber& nBig2)
 		nResult.integral.str = toString(nResult.integral.vec.crbegin(), nResult.integral.vec.crend());
 	}
 	nResult.negative = nBig1.negative != nBig2.negative;
-
-	//std::cout << nBig1.asString() << " : " << nBig2.asString() << " : " << nResult.asString() << '\n';
-
 	return nResult;
 }
-
--- BigNumber.hpp
+++ BigNumber.hpp
@@ -5,22 +5,27 @@
 
 class BigNumber {
 	static const int MAX_NUM = 9999;
-	static const int POWER_OF_10 = 10000;
+	static const int EXP_OF_10 = 10000;
+	static const int POWER_OF_10 = 4;
 public:
+	typedef int Int;
 	struct {
-		std::vector<int> vec;
-		std::string str;
+		std::vector<Int> vec = { 0 };
+		std::string str = "0";
 	} integral, fractional;
 
-	bool negative;
+	bool negative = false;
 
 	BigNumber();
 	BigNumber(int);
 	BigNumber(double);
-	BigNumber(std::string);
+	BigNumber(const std::string&);
 	~BigNumber();
 
 	std::string asString() const;
+	friend void fillVec(std::vector<BigNumber::Int>& vec, int x);
+	template<typename begin_it, typename end_it>
+	friend std::string toString(begin_it begin, end_it end, bool fractional);
 	friend std::ostream& operator<<(std::ostream&, const BigNumber&);
 
 	friend bool operator==(const BigNumber&, const BigNumber&);
@@ -33,9 +38,4 @@ public:
 	friend BigNumber operator+(const BigNumber&, const BigNumber&);
 	friend BigNumber operator-(const BigNumber&, const BigNumber&);
 	friend BigNumber operator*(const BigNumber&, const BigNumber&);
-
-private:
-	void setNumber(const int&);
-	void setNumber(const double&);
-	void setNumber(const std::string&);
 };
